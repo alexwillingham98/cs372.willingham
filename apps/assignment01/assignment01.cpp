50a434a02c2a9e50bf82109c6fa8011ef9da9eb9
alexwillingham98 <88211538+alexwillingham98@users.noreply.github.com>
2021-08-24 16:01:08 -0500

    Asg1
    updated code to ASG1

--- apps/assignment01/assignment01.cpp
+++ apps/assignment01/assignment01.cpp
@@ -1,6 +1,6 @@
 //
 // File:   assignment1.cpp
-// Author: <PUT YOUR NAME HERE>
+// Author: Alex Willingham
 // Purpose:
 // Illustrate some of the bad thins that can happen with
 // pointers
@@ -10,6 +10,29 @@ using namespace std;
 
 
 int main() {
-  // Put your code here.
+  //Problem A
+
+  int* firstP = new int [100000];
+  int* secondP = new int [100000];
+  //array delete of the first pointer
+  delete[] firstP;
+    for (int i = 0; i <= 10; i++)
+    {
+        cout << secondP[i];
+    }
+    cout << "\n";
+  //Problem B
+  int* newP = new int [200000];
+  int* newerP = new int [200000];
+  //regular delete of the first pointer
+  delete newP;
+    for (int b = 0; b <= 10; b++)
+    {
+        cout << newerP[b];
+    }
+    cout << "\n";
+  //Problem C
+
+
   return 0;
 }
