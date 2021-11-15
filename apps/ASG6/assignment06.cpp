6ce15ef0bc7a9be8241dbe887f127d976396e43a
alexwillingham98 <88211538+alexwillingham98@users.noreply.github.com>
2021-11-14 22:33:38 -0600

    ASG7
    update

--- apps/ASG6/assignment06.cpp
+++ apps/ASG6/assignment06.cpp
@@ -6,6 +6,7 @@
 //
 //
 #include<iostream>
+#include<chrono>
 //unfinished code
 /*inspired by https://www.chegg.com/homework-help/questions-and-answers/1-40-points-s-interesting-algorithm-int-equal-int-n-ado-int-j-k-1-q86120519
  given the type of code and how it iterates i would say the big o is O(1)*/
@@ -36,6 +37,7 @@ int anyEqual(int n, int A[50][20]) {
 // Driver code
 int main()
 {
+    clock_t start = clock();
     int A[] = { 10, 20, 30, 40, 50,100,500,1000 };
     int x = 20;
     int n = sizeof(A) / sizeof(A[0]);
@@ -44,6 +46,7 @@ int main()
     // Function call
     int result = anyEqual(n, A);
     (result == -1);
-    
-    return 0;
+    clock_t stop = clock();
+        double elapsed = (double) (stop - start) / CLOCKS_PER_SEC;
+        printf("\nTime elapsed: %.5f\n", elapsed);    return 0;
 }
